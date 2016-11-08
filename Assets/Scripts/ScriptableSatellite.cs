fca2013c6c563604eaa0c61e89abe21812a258ad
sreveloc <sreveloc@gmail.com>
2016-11-08 18:27:42 +0100

    Maquetación Modal Screen ScriptableObject de satelite

---
+++ Assets/Scripts/ScriptableSatellite.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[CreateAssetMenu (fileName = "Satellite", menuName = "New Satellite")]
+public class ScriptableSatellite : ScriptableObject {
+
+	public enum InfoType {
+		OPTICAL,
+		WEATHER_CONTROL,
+		WAR
+	}
+		
+	public string Excerpt;
+	public string Description;
+
+	[Header("Images")]
+	public Sprite NameImage;
+	public Sprite Picture;	
+
+	[Header("Info")]
+	public string Country;
+	public string Date;
+	public InfoType Type;
+	public string MassAtLaunch;
+
+	[ContextMenu("log")]
+	string GetTypeString() {
+		Debug.Log( Type.ToString ().Replace("_", " "));
+		return Type.ToString ().Replace("_", " ");
+	}
+}
